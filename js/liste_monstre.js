47c97a7442dcf884a6ccd36aa8aa7b64ff027d1c
valsimot <valsimot@gmail.com>
2016-03-18 06:14:20 -0400

    finish de la class monstre et liste

--- js/liste_monstre.js
+++ js/liste_monstre.js
@@ -1,46 +1,58 @@
-var bibliotheque_monstre = [new monstre("Rhela",2,1),
-             new monstre("Commercial",4,3),
-             new monstre("Canard Laqué",3,3)];
 
-function monstre(Nom,Vie,Attaque){
-  
-  this.Nom=Nom;
-  this.pointVie=Vie;
-  this.pointAttaque=Attaque;
-  this.pourcentageDrop=Drop;
+function monstre(Nom, Vie, Attaque) { //  Class monstre
+
+  this.Nom = Nom;
+  this.pointVie = Vie;
+  this.pointAttaque = Attaque;
   this.image;
   
+  this.getNom = function(){
+    return this.Nom;
+  }
+  
+  this.getpointVie = function(){
+    return this.pointVie;
+  }
+  
+  this.getpointAttaque = function(){
+    return this.pointAttaque;
+  }
+  
+  this.setpointVie = function(pointVie){
+    this.pointVie = pointVie;
+  }
+  
+  this.setpointAttaque = function(pointAttaque){
+    this.pointAttaque = pointAttaque;
+  }
+
 }
 
-function liste(){
+
+
+function liste() {  //  Class liste
+
+  this.bibliotheque_monstre = [new monstre("Rhela", 2, 1),
+                                new monstre("Commercial", 4, 3),
+                                new monstre("Canard Laqué", 3, 3)
+                              ];
+  this.liste_monstre = function() {
+
+                          var nombre_monstre = 3;
+                          var liste_monstre_choisi = [];
+
+                          for (var i = 0; i < nombre_monstre; i++) {
+
+                            var valeur = Math.round(Math.random() * (bibliotheque_monstre.length - 1));
+                            liste_monstre_choisi.push(bibliotheque_monstre[valeur]);
+
+                          }
+                          return liste_monstre_choisi;
+                        }
   
-  this.liste_monstre = creationListeAleatoire;
-  this.taille_liste = function (){
+  this.taille_liste = function() {
                         return this.liste_monstre.length;
                       }
-  
+
 }
-
-function creationListeAleatoire(){
-  
-  var nombre_monstre = 3;
-  var liste_monstre_choisi = [];
-  
-  for(var i=0;i<nombre_monstre;i++){
-    
-     var valeur = Math.round(Math.random()*(bibliotheque_monstre.length-1));
-     liste_monstre_choisi.push(bibliotheque_monstre[valeur]);
-    
-  }
- 
-  
- 
-  return liste_monstre_choisi;
-}
-
-
-
-
-
-
 
