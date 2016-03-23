8c80f22137227361c7cd52e2fa6ca6f8b376cd5f
jeremie71000 <jeremie71000@hotmail.fr>
2016-03-23 04:07:56 -0400

    ajout des images

--- js/view.js
+++ js/view.js
@@ -1,8 +1,8 @@
 var blocks = new Array();
 for (var i = 0; i < 6; i++) {
-    blocks.push();
+    blocks.push(new Array());
     for (var j = 0; j < 5; j++) {
-        blocks.push(new block(1, 0));
+        blocks[i].push(new case_(0, 0));
     }
 }
 
@@ -16,7 +16,13 @@ $(document).ready(function() {
         $('#board').append("<tr>");
         for (var j = 1; j <= 5; j++) {
 
-            $("#board").append("<td id='" + i + "_" + j + "'></td>")
+            if (!(blocks[i][j].getEst_revele())) {
+                $("#board").append("<td id='" + i + "_" + j + "' class='hidden'></td>");
+            } else if (blocks[i][j].getEst_destructible()) {
+                $("#board").append("<td id='" + i + "_" + j + "' class='revealed'></td>");
+            } else {
+                $("#board").append("<td id='" + i + "_" + j + "' class='broken'></td>");
+            }
 
         }
         $('#board').append("</tr>");
