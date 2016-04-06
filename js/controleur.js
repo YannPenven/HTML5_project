fe2197ac4b2faff4f624aef4221afb6e9522a89a
gs.yuri379 <gs.yuri379@gmail.com>
2016-04-06 03:11:11 -0400

    note

--- js/controleur.js
+++ js/controleur.js
@@ -21,18 +21,17 @@ $(".case").click(function(){  // Lors d'un clic sur une case
           if(Math.random() <= plateau_de_jeu.getTaux_monstre){   // Calcul si il y a un monstre ou non
 
               case_.setMonstre(new monstre("a",100,100));
-              
 
           }
 
-          
+          if(Math.random() <= plateau_de_jeu.getTaux_cle){    // Calcul si il y a une clé ou non
+
+
+
+          }
 
       }else{
 
-          if(case_.getMonstre!==false){
-            
-          }
-        
           alert("Coup interdit - La case ne peut pas être détruite");
 
       }    
@@ -41,4 +40,46 @@ $(".case").click(function(){  // Lors d'un clic sur une case
       
     }
   
+});
+
+$("document").ready(function(){
+
+  
+  var heroList = [];
+  var monstreList = [];
+  
+ 
+  $.ajax({
+  		type: "GET",
+  		url: "https://api.myjson.com/bins/2av9o",
+
+  		dataType : 'json',
+      contentType: "application/json; charset=utf-8",
+    
+  		success : function(json_file, statut){ // code_html contient le HTML renvoyé
+
+          console.log(json_file);
+           //var parsed_data = JSON.parse(json_file);
+           var hero_data = json_file.hero;
+           var monster_data = json_file.monstre;
+           for(var i in hero_data){
+              heroList.push(new hero(hero_data[i].life,hero_data[i].damage));//crée une liste des héro disponible
+            }
+           for(var j in monster_data){
+              monstreList.push(new monstre(monster_data[j].id,monster_data[j].life,monster_data[j].damage));//crée une liste des monstre disponible
+            }
+          //console.log(monstreList);
+          //affiche plateau
+   
+      },
+    
+      error : function(resultat, statut, erreur){
+          console.log("echec");
+       },
+  })
+
+  
+  //console.log(heroList);
+  
+
 });
